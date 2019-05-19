e359231e375cde3fb429e171cee086796f7004bc
GeorgeMarkham <george@gmarkham.com>
2019-05-19 11:59:04 +0100

    Done some theming

---
+++ Components/theme.js
@@ -0,0 +1,10 @@
+module.exports = {
+  colors:{
+    brand:"#3B86CC",
+    brand_dark: "#184E80",
+    accent1: "#FF5B59",
+    accent2:"#D55A53",
+    background: "#EEEEEE",
+    text: "#EEEEEE",
+  }
+}
